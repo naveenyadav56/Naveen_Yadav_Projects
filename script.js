f7279f7f32a8c202ee33f86bd9ad335286de57f8
Naveen Yadav <105793963+naveenyadav56@users.noreply.github.com>
2024-04-12 10:27:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("This script is running");
+document.querySelector('.cross').style.display = 'none';
+document.querySelector('.hamburger').addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebargo');
+    if (document.querySelector('.sidebar').classList.contains('sidebargo')){
+        document.querySelector('.ham').style.display = 'inline';
+        document.querySelector('.cross').style.display = 'none';
+    } else {
+        document.querySelector('.cross').style.display = 'none';
+        document.querySelector('.ham').style.display = 'inline';
+    }
+})
